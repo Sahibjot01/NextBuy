aa9dd17804e57cdf74b3f46feeaf187a989ed76a
sahibjot <sahibjot70@gmail.com>
2025-05-11 00:58:19 -0400

    fixed between date fn comparisons

--- app/dashboard/analytics/betweenDates.ts
+++ app/dashboard/analytics/betweenDates.ts
@@ -3,10 +3,8 @@ export const betweenDates = (dateToCheck: Date, start: number, end: number) => {
 
   const startDate = new Date(today);
   startDate.setDate(startDate.getDate() - start);
-  startDate.setHours(0, 0, 0, 0);
   const endDate = new Date(today);
   endDate.setDate(endDate.getDate() - end);
-  endDate.setHours(0, 0, 0, 0);
 
   return dateToCheck <= startDate && dateToCheck >= endDate;
 };
