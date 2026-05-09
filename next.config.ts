6a2794517be8ddefd1fe19d206edd6c799ec89cb
sahibjot <sahibjot70@gmail.com>
2026-05-08 20:03:25 -0400

    Silence Next root warning

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingRoot: process.cwd(),
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
